080110da81b9fe3ae2f898de693ebf74119ac828
Tom Workman <tworkman512@gmail.com>
2019-07-14 15:53:49 -0500

    feature(Books): Sets up successful PUT and DELETE methods for book by id

--- api/routes/books.js
+++ api/routes/books.js
@@ -2,30 +2,8 @@ const router = require("express").Router();
 const { generate: generateId } = require("shortid");
 const Book = require("../models/book");
 
-// const books = [
-//   {
-//     id: 'j9U3iNIQi',
-//     title: 'The Colour of Magic',
-//     published: 1983,
-//     authors: [
-//       {
-//         name: 'Sir Terry Pratchett',
-//         dob: '04-28-1948'
-//       }
-//     ]
-//   },
-//   {
-//     id: 'ubQnXOfJV',
-//     title: 'Stardust',
-//     published: 1997,
-//     authors: [
-//       {
-//         name: 'Neil Gaiman',
-//         dob: '11-10-1960'
-//       }
-//     ]
-//   }
-// ];
+// SAVE FOR .select() LATER
+// const publicKeys = '_id title published authors -__v'
 
 router.get("/", async (req, res, next) => {
   const status = 200;
@@ -54,23 +32,18 @@ router.get("/:id", async (req, res, next) => {
   res.json({ status, response });
 });
 
-router.put("/:id", (req, res, next) => {
+router.put("/:id", async (req, res, next) => {
   const status = 200;
-  const response = { id: req.params.id, ...req.body };
-  const single = books.find(({ id }) => id === req.params.id);
-  const index = books.indexOf(single);
-
-  books.splice(index, 1, response);
+  const query = { _id: req.params.id };
+  const options = { new: true };
+  const response = await Book.findOneAndUpdate(query, req.body, options);
 
   res.json({ status, response });
 });
 
-router.delete("/:id", (req, res, next) => {
+router.delete("/:id", async (req, res, next) => {
   const status = 200;
-  const response = books.find(({ id }) => id === req.params.id);
-  const index = books.indexOf(response);
-
-  books.splice(index, 1);
+  const response = await Book.findByIdAndDelete({ _id: req.params.id });
 
   res.json({ status, response });
 });

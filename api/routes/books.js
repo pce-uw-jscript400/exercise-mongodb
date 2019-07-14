1ccbd55b44dc92d63913a03fb7b809f220d57a5a
Alycia Cahill <acahil@uw.edu>
2019-07-14 15:15:52 -0700

    Homework for Submission

--- api/routes/books.js
+++ api/routes/books.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const { generate: generateId } = require('shortid')
+const Books = require('../models/books')
 
 const books = [
   {
@@ -26,48 +27,101 @@ const books = [
   }
 ];
 
-router.get('/', (req, res, next) => {
+router.get('/', async (req, res, next) => {
   const status = 200
-  const response = books
-  
+  const response = await Books.find()
   res.json({ status, response })
 })
 
-router.post('/', (req, res, next) => {
+router.post('/', async (req, res, next) => {
   const status = 201
-  
-  books.push({ id: generateId(), ...req.body })
-  const response = books
-  
-  res.json({ status, response })
+  try{
+    const response = await Books.create(req.body)
+    res.json({status, response})
+  }catch(error){
+    console.error(error)
+    const e = new Error('Opps! Something went wrong')
+    e.status = 400
+    next(e)
+  }
 })
 
-router.get('/:id', (req, res, next) => {
+router.get('/:id', async (req, res, next) => {
   const status = 200
-  const response = books.find(({ id }) => id === req.params.id)
-
+  const id = req.params.id;
+  const response = await Books.findOne({_id: id})
   res.json({ status, response })
 })
 
-router.put('/:id', (req, res, next) => {
+router.put('/:id', async (req, res, next) => {
   const status = 200
-  const response = { id: req.params.id, ...req.body }
-  const single = books.find(({ id }) => id === req.params.id)
-  const index = books.indexOf(single)
-
-  books.splice(index, 1, response)
-  
+  const id = req.params.id;
+  const title = req.body.title;
+  const pub = req.body.published;
+  const response = await Books.findOneAndUpdate({_id: id}, {title: title, published: pub}, {new: true})
   res.json({ status, response })
 })
 
-router.delete('/:id', (req, res, next) => {
+router.delete('/:id', async (req, res, next) => {
   const status = 200
-  const response = books.find(({ id }) => id === req.params.id)
-  const index = books.indexOf(response)
-
-  books.splice(index, 1)
-
+  response = await Books.findOneAndDelete({_id: req.params.id});
   res.json({ status, response })
 })
+
+router.get('/:id/authors', async (req, res, next) => {
+  const status = 200
+  const id = req.params.id;
+  const parent = await Books.findOne({_id: id})
+  const response = parent.authors;
+  res.json({ status, response })
+})
+
+//GET /api/books/:bookId/authors/:authorId
+router.get('/:id/authors/:authorId', async (req, res, next) => {
+  const status = 200
+  const id = req.params.id;
+  const parent = await Books.findOne({_id: id})
+  const authorId = req.params.authorId
+  const response = parent.authors.id(authorId)
+  res.json({ status, response })
+})
+
+//POST /api/books/:bookId/authors
+router.post('/:id/authors', async (req, res, next) => {
+  const status = 200
+  const id = req.params.id;
+  const book = await Books.findOne({_id: id})
+  const newAuthor = book.authors.push(req.body)
+  newAuthor.isNew
+  book.save()
+  const response = book.authors
+  res.json({ status, response })
+})
+
+//PUT /api/books/:bookId/authors/:authorId
+router.put('/:id/authors/:authorId', async (req, res, next) => {
+  const status = 200
+  const id = req.params.id;
+  const authorId = req.params.authorId
+  const book= await Books.findOne({_id: id})
+  const author = book.authors.id(authorId)
+  author.set(req.body)
+  book.save()
+  response = author
+  res.json({ status, response })
+})
+
+
+//DELETE /api/books/:bookId/authors/:authorId
+router.delete('/:id/authors/:authorId', async (req, res, next) => {
+  const status = 200
+  const id = req.params.id;
+  const book = await Books.findOne({_id: id})
+  const authorId = req.params.authorId
+  book.authors.id(authorId).remove()
+  const response = book
+  res.json({ status, response })
+})
+
 
 module.exports = router

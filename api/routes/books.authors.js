c382f4bd627245b8f38731a3b3898ce0e3f639b9
Tom Workman <tworkman512@gmail.com>
2019-07-14 21:29:56 -0500

    feature(Authors): Sets up new authors route and also creates GET, POST, PUT, DELETE methods for the authors

--- api/routes/books.authors.js
+++ api/routes/books.authors.js
@@ -1,18 +1,22 @@
 const router = require("express").Router({ mergeParams: true });
 const Book = require("../models/book");
 
+const publicKeys = "_id title published authors -__v";
+
 router.get("/", async (req, res, next) => {
   const status = 200;
-  const { authors } = await Book.findById(req.params.bookId).select("authors");
+  const { authors } = await Book.findById(req.params.bookId)
+    .select("authors")
+    .select(publicKeys);
   res.json({ status, authors });
 });
 
 router.post("/", async (req, res, next) => {
   const status = 201;
-  const book = await Book.findById(req.params.bookId);
+  const book = await Book.findById(req.params.bookId).select(publicKeys);
 
   book.authors.push(req.body);
-  await book.save();
+  await books.save();
 
   const author = book.authors[book.authors.length - 1];
   res.status(status).json({ status, author });
@@ -22,7 +26,7 @@ router.put("/:id", async (req, res, next) => {
   const status = 200;
   const book = await Book.findById(req.params.bookId);
 
-  const author = book.authors.id(req.params.id);
+  const author = book.author.id(req.params.id);
   Object.assign(author, req.body);
   await book.save();
 
--- api/routes/books.js
+++ api/routes/books.js
@@ -2,12 +2,11 @@ const router = require("express").Router();
 const { generate: generateId } = require("shortid");
 const Book = require("../models/book");
 
-// SAVE FOR .select() LATER
-// const publicKeys = '_id title published authors -__v'
+const publicKeys = "_id title published authors -__v";
 
 router.get("/", async (req, res, next) => {
   const status = 200;
-  const response = await Book.find();
+  const response = await Book.find().select(publicKeys);
 
   res.json({ status, response });
 });
@@ -15,7 +14,7 @@ router.get("/", async (req, res, next) => {
 router.post("/", async (req, res, next) => {
   const status = 201;
   try {
-    const response = await Book.create(req.body);
+    const response = await Book.create(req.body).select(publicKeys);
     res.json({ status, response });
   } catch (error) {
     console.log(error);
@@ -27,7 +26,7 @@ router.post("/", async (req, res, next) => {
 
 router.get("/:id", async (req, res, next) => {
   const status = 200;
-  const response = await Book.findById(req.params.id);
+  const response = await Book.findById(req.params.id).select(publicKeys);
 
   res.json({ status, response });
 });
@@ -36,14 +35,18 @@ router.put("/:id", async (req, res, next) => {
   const status = 200;
   const query = { _id: req.params.id };
   const options = { new: true };
-  const response = await Book.findOneAndUpdate(query, req.body, options);
+  const response = await Book.findOneAndUpdate(query, req.body, options).select(
+    publicKeys
+  );
 
   res.json({ status, response });
 });
 
 router.delete("/:id", async (req, res, next) => {
   const status = 200;
-  const response = await Book.findByIdAndDelete({ _id: req.params.id });
+  const response = await Book.findByIdAndDelete({ _id: req.params.id }).select(
+    publicKeys
+  );
 
   res.json({ status, response });
 });

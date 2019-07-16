57ba59c27eac6ebe87dab0b32fcfbc77385a6a95
Somanath Krishnaswamy <soma17@uw.edu>
2019-07-16 10:58:00 -0700

    Completed excersice

--- api/routes/books.js
+++ api/routes/books.js
@@ -323,7 +323,6 @@ router.get('/:bookId/authors', async (req, res, next) => {
     ]
 }
  */
-
 router.get('/:bookId/authors/:authorId', async (req, res, next) => {
   const status = 200
   const response = await Books.find( {"_id" :req.params.bookId} , {authors : {$elemMatch : {_id : req.params.authorId}}})
@@ -331,13 +330,114 @@ router.get('/:bookId/authors/:authorId', async (req, res, next) => {
   res.json({status, response})
 })
 
+// Find a book and add a new author to an existing book then we can use this api
+// Lets say I want to add a new author (Soma) to an existing book Writing Secure Code I can use this api.
+// The state of books before the call is as mentioned in the previous comment.
+/*
+{
+    "status": 200,
+    "response": [
+        {
+            "_id": "BSgAN5ysK",
+            "title": "Becoming",
+            "published": 2018,
+            "authors": [
+                {
+                    "_id": "5d2dd5dd197e0336a8d43b23",
+                    "name": "Michelle Obama",
+                    "dob": "01-07-1964"
+                }
+            ],
+            "created_at": "2019-07-16T13:49:17.421Z",
+            "updated_at": "2019-07-16T13:49:17.421Z",
+            "__v": 0
+        },
+        {
+            "_id": "kRTOw6SK4",
+            "title": "Writing Secure Code",
+            "published": 2001,
+            "authors": [
+                {
+                    "_id": "5d2dd8d2f8c5e33a28520b10",
+                    "name": "David LeBlanc"
+                },
+                {
+                    "_id": "5d2dd8d2f8c5e33a28520b0f",
+                    "name": "Michael Howard"
+                },
+                {
+                    "_id": "5d2e01242210753bc82354a2",
+                    "name": "Soma"
+                }
+            ],
+            "created_at": "2019-07-16T13:59:22.704Z",
+            "updated_at": "2019-07-16T16:53:56.545Z",
+            "__v": 0
+        }
+    ]
+}
+ */
 router.post('/:bookId/authors', async (req, res, next) =>{
   const status = 201
-  const response = await Books.findByIdAndUpdate(req.params.bookId, {$push : {"authors" : req.body}}, {new : true})
-
-  res.json({status, response})
+  try {
+    const response = await Books.findByIdAndUpdate(req.params.bookId, {$push : {"authors" : req.body}}, {new : true})
+    res.json({status, response})
+  }
+  catch(error) {
+    console.error(error);
+    const e = new Error('Something went wrong, Unable to POST');
+    e.status = 400;
+    return(e);
+  }
 })
 
+// Now lets say I want to update the details of an existing autor for a specific book. I would do it using this
+// put request. The book collection is as it was in the previous comment. Now lets say I want to update the name of one of the authors
+// I want to update autor 3 from Soma to Somanath, then I would use this request. The GET request after this operation returns
+/*
+{
+    "status": 200,
+    "response": [
+        {
+            "_id": "BSgAN5ysK",
+            "title": "Becoming",
+            "published": 2018,
+            "authors": [
+                {
+                    "_id": "5d2dd5dd197e0336a8d43b23",
+                    "name": "Michelle Obama",
+                    "dob": "01-07-1964"
+                }
+            ],
+            "created_at": "2019-07-16T13:49:17.421Z",
+            "updated_at": "2019-07-16T13:49:17.421Z",
+            "__v": 0
+        },
+        {
+            "_id": "kRTOw6SK4",
+            "title": "Writing Secure Code",
+            "published": 2001,
+            "authors": [
+                {
+                    "_id": "5d2dd8d2f8c5e33a28520b10",
+                    "name": "David LeBlanc"
+                },
+                {
+                    "_id": "5d2dd8d2f8c5e33a28520b0f",
+                    "name": "Michael Howard"
+                },
+                {
+                    "_id": "5d2e0d6190ed7c424c210857",
+                    "name": "Somanath"
+                }
+            ],
+            "created_at": "2019-07-16T13:59:22.704Z",
+            "updated_at": "2019-07-16T17:46:09.785Z",
+            "__v": 0
+        }
+    ]
+}
+ */
 router.put('/:bookId/authors/:authorId', async (req, res, next) => {
   const status = 200
   const response = await Books.updateOne({"_id" : req.params.bookId, "authors._id" : req.params.authorId}, {$set : {"authors.$" : req.body}}, {new : true})
@@ -345,13 +445,53 @@ router.put('/:bookId/authors/:authorId', async (req, res, next) => {
   res.json({status, response})
 })
 
+// Now lets say I want to delete a specific autor from the book collection. Then I will use this API
+// The GET request after executing this call returns
+/*
+{
+    "status": 200,
+    "response": [
+        {
+            "_id": "BSgAN5ysK",
+            "title": "Becoming",
+            "published": 2018,
+            "authors": [
+                {
+                    "_id": "5d2dd5dd197e0336a8d43b23",
+                    "name": "Michelle Obama",
+                    "dob": "01-07-1964"
+                }
+            ],
+            "created_at": "2019-07-16T13:49:17.421Z",
+            "updated_at": "2019-07-16T13:49:17.421Z",
+            "__v": 0
+        },
+        {
+            "_id": "kRTOw6SK4",
+            "title": "Writing Secure Code",
+            "published": 2001,
+            "authors": [
+                {
+                    "_id": "5d2dd8d2f8c5e33a28520b10",
+                    "name": "David LeBlanc"
+                },
+                {
+                    "_id": "5d2dd8d2f8c5e33a28520b0f",
+                    "name": "Michael Howard"
+                }
+            ],
+            "created_at": "2019-07-16T13:59:22.704Z",
+            "updated_at": "2019-07-16T17:56:12.243Z",
+            "__v": 0
+        }
+    ]
+}
+ */
 router.delete('/:bookId/authors/:authorId', async (req, res, next) => {
   const status = 200
   console.log(req.params.bookId)
   console.log(req.params.authorId)
-  const response = await Books.updateOne({"_id" : req.params.bookId, "authors._id" : req.params.authorId}, {$pull : {"authors.$" : 1}}, {new : true})
- // const response = await Books.updateOne({"_id" : req.params.bookId, "author._id" : req.params.authorId}, {$pull : {"author.$" : 1}}, {new : true})
-  //const final = await Books.updateOne({}, {$pull:{"author.$":null}})
+  const response = await Books.updateOne({"_id" : req.params.bookId}, {$pull : {"authors" : {"_id" : req.params.authorId}}}, {new : true}) 
 
   res.json({status, response})
 })

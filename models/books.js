e4bab7343708d4d2b381db29ff6f982302d1c75e
Erik <oieeri26@uw.edu>
2019-07-16 19:15:26 -0700

    week 3 exercise

---
+++ models/books.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var schema = new Schema({
+    title: { 
+        type: String, 
+        required: true 
+    },
+    published: { 
+        type: Number, 
+        required: true 
+    },
+    authors: [
+        {
+            name: String,
+            dob: String
+        }
+    ]}, {
+        timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' }
+    });
+
+module.exports = mongoose.model('Books', schema)

4cafa3f4f56a3edfec5e044fea523e03431cdcea
Laura_MS <lmayers3@uw.edu>
2019-07-16 23:01:47 -0700

    fixed a couple more

--- api/routes/books.js
+++ api/routes/books.js
@@ -2,31 +2,6 @@ const router = require('express').Router()
 const { generate: generateId } = require('shortid')
 const Books = require('../model/books')
 
-const books = [
-  {
-    id: 'j9U3iNIQi',
-    title: 'The Colour of Magic',
-    published: 1983,
-    authors: [
-      {
-        name: 'Sir Terry Pratchett',
-        dob: '04-28-1948'
-      }
-    ]
-  },
-  {
-    id: 'ubQnXOfJV',
-    title: 'Stardust',
-    published: 1997,
-    authors: [
-      {
-        name: 'Neil Gaiman',
-        dob: '11-10-1960'
-      }
-    ]
-  }
-];
-
 //works
 router.get('/', async (req, res, next) => {
   const status = 200
@@ -51,7 +26,7 @@ router.get('/:bookId/authors', async (req,res,next) => {
 //doesn't work
 router.get('/:bookId/authors/:authorId', async (req, res, next) => {
   const status = 200
-  const response = await Books.findOne({_id:req.params.bookId})
+  const response = await Books.findOne({authorId:req.params.authorId})
     res.json({ status, doc })
 })
 
@@ -62,17 +37,18 @@ router.post('/', (req, res, next) => {
   res.json({ status, response })
 })
 
-//doesn't work
+//works
 router.post('/:bookId/authors', async (req, res, next) => {
   const status = 201
-  const response = await Books.findByIdAndUpdate(req.params.bookId,req.body)
+  console.log(req.body.name, req.body.dob);
+  const response = await Books.findByIdAndUpdate(req.params.bookId, { authors: { name:req.body.name, dob:req.body.dob } } )
   res.json({ status, response })
 })
 
 //works
 router.put('/:bookId', async (req, res, next) => {
   const status = 200
-  const response = await Books.findOneAndUpdate({ _id: req.params.bookId }, { title: req.body.title, published:req.body.published, authors:req.body.authors }, 
+  const response = await Books.findOnAndUpdate({ _id: req.params.bookId }, { title: req.body.title, published:req.body.published, authors:req.body.authors }, 
     { new:true, omitUndefined:true })
   res.json({ status, response })
 })
@@ -80,8 +56,8 @@ router.put('/:bookId', async (req, res, next) => {
 //update an author of a certain book
 router.put('/:bookId/authors/:authorId', async (req, res, next) => {
   const status = 200
-  const response = await Books.findOneAndUpdate({ _id:req.params.bookId }, 
-    { title: req.body.title, published:req.body.published, authors:req.body.authors }, 
+  const response = await Books.findOneAndUpdate({ authorId:req.params.authorId }, 
+    { name: req.body.name, dob:req.body.dob }, 
     { new:true, omitUndefined:true })
   res.json({ status, response })
 })
@@ -96,7 +72,8 @@ router.delete('/:bookId', async (req, res, next) => {
 //delete an author of a certain book
 router.delete('/:bookId/authors/:authorId', (req, res, next) => {
   const status = 200
-  const response = Books.findOneAndDelete({ authorId:req.params.authorId})
+  console.log(req.params.authorId);
+  const response = Books.findOneAndDelete({authorId:req.params.authorId})
   res.json({ status, response })
 })
 

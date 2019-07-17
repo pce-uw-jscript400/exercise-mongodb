279bee71d6f3ca2ff38703eb661956f165f94182
Jonathan Veilleux <wedge1126@gmail.com>
2019-07-16 17:09:10 -0700

    books api

--- api/routes/books.js
+++ api/routes/books.js
@@ -1,71 +1,40 @@
 const router = require('express').Router()
-const { generate: generateId } = require('shortid')
+const Books = require('../models/book')
 
-const books = [
-  {
-    id: 'j9U3iNIQi',
-    title: 'The Colour of Magic',
-    published: 1983,
-    authors: [
-      {
-        name: 'Sir Terry Pratchett',
-        dob: '04-28-1948'
-      }
-    ]
-  },
-  {
-    id: 'ubQnXOfJV',
-    title: 'Stardust',
-    published: 1997,
-    authors: [
-      {
-        name: 'Neil Gaiman',
-        dob: '11-10-1960'
-      }
-    ]
-  }
-];
-
-router.get('/', (req, res, next) => {
+router.get('/', async (req, res, next) => {
   const status = 200
-  const response = books
+  const response = await Books.find().select('_id title published authors')
   
   res.json({ status, response })
 })
 
-router.post('/', (req, res, next) => {
+router.post('/', async (req, res, next) => {
   const status = 201
   
-  books.push({ id: generateId(), ...req.body })
-  const response = books
+  const { _id, _title, published, authors } = await Books.create(req.body)
+  const response = { _id, _title, published, authors }
+  
+  res.status(status).json({ status, response })
+})
+
+router.get('/:id', async (req, res, next) => {
+  const status = 200
+  const response = await Books.findById(req.params.id).select('_id title published authors')
+
+  res.json({ status, response })
+})
+
+router.put('/:id', async (req, res, next) => {
+  const status = 200
+  const response = await Books.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true })
+    .select('_id title published authors')
   
   res.json({ status, response })
 })
 
-router.get('/:id', (req, res, next) => {
+router.delete('/:id', async (req, res, next) => {
   const status = 200
-  const response = books.find(({ id }) => id === req.params.id)
-
-  res.json({ status, response })
-})
-
-router.put('/:id', (req, res, next) => {
-  const status = 200
-  const response = { id: req.params.id, ...req.body }
-  const single = books.find(({ id }) => id === req.params.id)
-  const index = books.indexOf(single)
-
-  books.splice(index, 1, response)
-  
-  res.json({ status, response })
-})
-
-router.delete('/:id', (req, res, next) => {
-  const status = 200
-  const response = books.find(({ id }) => id === req.params.id)
-  const index = books.indexOf(response)
-
-  books.splice(index, 1)
+  const response = await Books.findOneAndDelete({ _id: req.params.id }).select('_id title published authors')
 
   res.json({ status, response })
 })
